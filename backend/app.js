ac033953965cc86f222be60276bdfbadce87b948
sachchida nand kumar <kumarsachchidanand4@gmail.com>
2024-02-23 16:23:11 +0530

    changes 3

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const app=express();
 const port=process.env.PORT ||8080;
-const mongoose=require('mongoose')
+const mongoose=require('mongoose') 
 const seedDB=require('./seed')
 const quotRoutes=require('./apis/quoteRoute')
 const cors=require('cors')
@@ -26,7 +26,7 @@ app.get("/hello",(req,res)=>{
 })
 app.use(express.urlencoded({extended:true})) 
 app.use(express.json());
-app.use(cors({origin:['http://localhost:3000']}))
+app.use(cors({origin:['https://quotes-web-app-12.onrender.com']}))
 app.use(quotRoutes)
 
 //seedDB();
